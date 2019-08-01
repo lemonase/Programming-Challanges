a040288a8c56f449fd6b9ce7690b633b87d03b77
James Dixon <itzjamesx@gmail.com>
2019-07-31 21:13:18 -0400

    05 smalled multiple solved

--- project-euler/05.py
+++ project-euler/05.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python3
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def smallestMultiple(small):
+    i = 1
+    while i < 21:
+        while small % i != 0:
+            small += 1
+            i = 1
+        i += 1
+
+    print(small)
+
+
+smallestMultiple(1)
