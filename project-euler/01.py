3e7f999c00270250554f700dd86da1f5250b3f33
James Dixon <itzjamesx@gmail.com>
2019-08-23 20:16:58 -0400

    Cleaned up solution

--- project-euler/01.py
+++ project-euler/01.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
-
-sum = 0
-
-for ind in range(1000):
-    if ind % 3 == 0:
-        sum = sum + ind
-    if ind % 5 == 0:
-        sum = sum + ind
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, # we get 3, 5, 6 and 9.
+# The total of these multiples is 23.
+# Find the total of all the multiples of 3 or 5 below 1000.
 
 
-print(sum)
+def sum_of_multiples(upper):
+    total = 0
+
+    for ind in range(upper):
+        if ind % 3 == 0:
+            total = total + ind
+        if ind % 5 == 0:
+            total = total + ind
+
+    print("total of multiples:", total)
+
+
+sum_of_multiples(10)
+sum_of_multiples(1000)
