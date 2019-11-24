86fdee817e81b6a681810c8d1e2daf619721a752
James Dixon <notjamesdixon@gmail.com>
2019-11-24 16:10:32 -0500

    solve problem 7-9

--- project-euler/09.py
+++ project-euler/09.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python3
+
+def main():
+    a = 3
+    b = 4
+    c = (a**2 * b**2)
+    product = a * b * c
+
+    while product != 1000:
+        if (a**2 + b**2 != c**2):
+            pass
+        elif (a**2 + b**2 == c**2):
+            product = a * c * b
+            print(product)
+
+main()
